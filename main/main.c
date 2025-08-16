d2421e9e84882a69f356112872fbccab6ca85283
zongxin <zongxin@dlkede.com>
2025-08-16 10:40:37 +0800

    Fix: Tidy up base project

---
+++ main/main.c
@@ -0,0 +1,16 @@
+#include "esp_log.h"
+#include "freertos/idf_additions.h"
+#include "hid_host.h"
+
+void app_main(void) {
+
+    hid_host_init();
+
+    while (1) {
+        if (xQueueReceive(get_queue_handle(), get_event_queue(), portMAX_DELAY)) {
+            if (APP_EVENT_HID_HOST ==  get_event_queue()->event_group) {
+                hid_host_device_event(get_event_queue());
+            }
+        }
+    }
+}
